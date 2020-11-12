8a2ae2785c41c0042813f279730ac7e6fefbbcbe
Mirrashad Hasanov <mirrashadhasanov@mail.ru>
2020-11-12 17:08:04 +0400

    oldu

--- src/main/java/com/herokuYuklemeyeCalisiriq/calisdigimizYer.java
+++ src/main/java/com/herokuYuklemeyeCalisiriq/calisdigimizYer.java
@@ -1,6 +1,7 @@
 package com.herokuYuklemeyeCalisiriq;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,9 @@ public class calisdigimizYer {
         return adlarrepo.findAll();
     }
 
+    @PostMapping
+    adlar save(adlar adlar){
+        return adlarrepo.save(adlar);
+    }
 
 }
